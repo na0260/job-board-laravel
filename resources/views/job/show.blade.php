c2e38464416248bb6ab9b6b16403284a82d8aab0
na0260 <samjr.008@gmail.com>
2024-03-22 20:51:00 +0600

    breadcrumbs added

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,3 +1,10 @@
 <x-layout>
+    <x-breadcrumbs
+        class="mb-4"
+        :links="[
+    'jobs'=>route('jobs.index'),
+    $job->title=>'#'
+    ]"
+    />
     <x-job-card :$job/>
 </x-layout>
