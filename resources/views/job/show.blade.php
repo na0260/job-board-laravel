8677213cb7e40adb7397e25268b5a83558128ed4
na0260 <samjr.008@gmail.com>
2024-03-23 01:03:11 +0600

    modified

--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -6,5 +6,9 @@
     $job->title=>'#'
     ]"
     />
-    <x-job-card :$job/>
+    <x-job-card :$job>
+        <p class="mb-4 text-sm text-slate-500">
+            {!! nl2br(e($job->description)) !!}
+        </p>
+    </x-job-card>
 </x-layout>
