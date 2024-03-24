735b959e11c905fbed787ac0781fe2910c084a39
na0260 <samjr.008@gmail.com>
2024-03-25 01:05:27 +0600

    form id added

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -6,17 +6,17 @@
     ]"
     />
     <x-card class="mb-4 text-xs">
-        <form action="{{route('jobs.index')}}" method="GET">
+        <form id="filtering-form" action="{{route('jobs.index')}}" method="GET">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Search</div>
-                    <x-text-input name="search" value="{{request('search')}}" placeholder="Search for any text"/>
+                    <x-text-input name="search" form-id="filtering-form" value="{{request('search')}}" placeholder="Search for any text"/>
                 </div>
                 <div>
                     <div class="mb-1 font-semibold">Salary</div>
                     <div class="flex space-x-2">
-                        <x-text-input name="min_salary" value="{{request('min_salary')}}" placeholder="From"/>
-                        <x-text-input name="max_salary" value="{{request('max_salary')}}" placeholder="To"/>
+                        <x-text-input name="min_salary" form-id="filtering-form" value="{{request('min_salary')}}" placeholder="From"/>
+                        <x-text-input name="max_salary" form-id="filtering-form" value="{{request('max_salary')}}" placeholder="To"/>
                     </div>
                 </div>
                 <div>
