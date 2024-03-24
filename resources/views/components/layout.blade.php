6e8fe3b94aa7620069b8228ed5c211104a8fca3f
na0260 <samjr.008@gmail.com>
2024-03-25 01:05:36 +0600

    bg color added

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
     <title>Laravel Job Board</title>
     @vite('resources/css/app.css')
 </head>
-<body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
+<body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200 text-slate-700">
 {{ $slot}}
 </body>
 </html>
